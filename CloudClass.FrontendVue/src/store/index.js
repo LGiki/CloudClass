9b54e9358277605bbec97575d73caeecacad9380
LGiki <qq547221432@gmail.com>
2021-04-02 09:21:52 +0800

    Frontend: Add response interceptor and store for JWT auth

--- CloudClass.FrontendVue/src/store/index.js
+++ CloudClass.FrontendVue/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import token from "./modules/token";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    token,
+  },
 });
