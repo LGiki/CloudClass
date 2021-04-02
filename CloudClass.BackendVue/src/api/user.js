7cae889ba0afb9dac44866f9ffa0d200468afcd6
LGiki <qq547221432@gmail.com>
2021-04-02 09:41:28 +0800

    WIP: frontend API call

--- CloudClass.BackendVue/src/api/user.js
+++ CloudClass.BackendVue/src/api/user.js
@@ -1,24 +1,27 @@
 import request from '@/utils/request'
 
-export function login(data) {
+// export function login(data) {
+//   return request({
+//     url: '/vue-admin-template/user/login',
+//     method: 'post',
+//     data
+//   })
+// }
+
+export function login(username, password) {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: '/user/login',
     method: 'post',
-    data
+    data: {
+      username: username,
+      password: password
+    }
   })
 }
 
-export function getInfo(token) {
+export function getInfo() {
   return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
-
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
+    url: '/user/info',
+    method: 'get'
   })
 }
