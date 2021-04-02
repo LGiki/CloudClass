a2bc8840ff5ddd7e01c9c9fb9d1aed2dcf2c9014
LGiki <qq547221432@gmail.com>
2021-04-02 09:13:37 +0800

    Add response interceptor and store for JWT auth

---
+++ CloudClass.BackendVue/src/store/modules/token.js
@@ -0,0 +1,31 @@
+const state = {
+  token: ''
+}
+
+const mutations = {
+  SET_TOKEN(state, token) {
+    state.token = token
+    localStorage.setItem('token', token)
+  },
+  REMOVE_TOKEN(state) {
+    state.token = ''
+    localStorage.removeItem('token')
+  }
+}
+
+const actions = {
+  setToken({ commit }) {
+    commit('SET_TOKEN')
+  },
+  removeToken({ commit }) {
+    commit('REMOVE_TOKEN')
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
