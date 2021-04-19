2e5d8495b6b0200896c44f57876c4119cd6d89b8
oneo7Lab <54404029+oneo7Lab@users.noreply.github.com>
2021-04-19 23:45:01 +0800

    Add files via upload

--- CloudClass.FrontendVue/src/views/Login.vue
+++ CloudClass.FrontendVue/src/views/Login.vue
@@ -1,80 +1,88 @@
 <template>
-  <div style="height: 100%;background-color: rgba(0,0,0,.02)" class="d-flex align-center">
+  <div
+    style="height: 100%; background-color: rgba(0, 0, 0, 0.02)"
+    class="d-flex align-center"
+  >
     <v-container>
-      <h1 style="font-size: 2.5em; color: #1976d2" class="d-flex justify-center ma-5">到云APP - 登录</h1>
+      <h1
+        style="font-size: 2.5em; color: #1976d2"
+        class="d-flex justify-center ma-5"
+      >
+        到云APP - 登录
+      </h1>
       <v-text-field
-          prepend-icon="mdi-account"
-          v-model="username"
-          label="用户名"
-          outlined
-          v-if="messageLogin === false"
+        prepend-icon="mdi-account"
+        v-model="username"
+        label="用户名"
+        outlined
+        v-if="messageLogin === false"
       ></v-text-field>
       <v-text-field
-          prepend-icon="mdi-lock"
-          v-model="password"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required]"
-          :type="showPassword ? 'text' : 'password'"
-          label="密码"
-          outlined
-          @click:append="showPassword = !showPassword"
-          v-if="messageLogin === false"
+        prepend-icon="mdi-lock"
+        v-model="password"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[rules.required]"
+        :type="showPassword ? 'text' : 'password'"
+        label="密码"
+        outlined
+        @click:append="showPassword = !showPassword"
+        v-if="messageLogin === false"
       ></v-text-field>
       <v-text-field
-          prepend-icon="mdi-account"
-          v-model="username"
-          :rules="[rules.required, rules.phone]"
-          label="手机号"
-          outlined
-          v-if="messageLogin === true"
+        prepend-icon="mdi-account"
+        v-model="phone"
+        :rules="[rules.required, rules.phone]"
+        label="手机号"
+        outlined
+        v-if="messageLogin === true"
       ></v-text-field>
       <div class="d-flex">
         <v-text-field
-            :rules="[rules.required]"
-            prepend-icon="mdi-android-messages"
-            v-model="verifyCode"
-            label="验证码"
-            outlined
-            v-if="messageLogin === true"
+          :rules="[rules.required]"
+          prepend-icon="mdi-android-messages"
+          v-model="verifyCode"
+          label="验证码"
+          outlined
+          v-if="messageLogin === true"
         ></v-text-field>
         <v-btn
-            class="ma-2"
-            color="primary"
-            large
-            v-if="messageLogin === true"
-            @click="getVerifyCode"
-        >获取验证码
-        </v-btn
-        >
+          class="ma-2"
+          color="primary"
+          large
+          v-if="messageLogin === true"
+          @click="getVerifyCode"
+          >获取验证码
+        </v-btn>
       </div>
 
       <div class="d-flex justify-end">
         <v-btn
-            class="ma-2 float-left"
-            elevation="0"
-            color="primary"
-            small
-            v-if="messageLogin === false"
-            @click="messageLogin = true"
-        >切换短信登录
-        </v-btn
-        >
+          class="ma-2 float-left"
+          elevation="0"
+          color="primary"
+          small
+          v-if="messageLogin === false"
+          @click="messageLogin = true"
+          >切换短信登录
+        </v-btn>
         <v-btn
-            class="ma-2 float-left"
-            elevation="0"
-            color="primary"
-            small
-            @click="messageLogin = false"
-            v-if="messageLogin === true"
-        >切换账号密码登录
-        </v-btn
-        >
+          class="ma-2 float-left"
+          elevation="0"
+          color="primary"
+          small
+          @click="messageLogin = false"
+          v-if="messageLogin === true"
+          >切换账号密码登录
+        </v-btn>
         <v-btn class="ma-2" elevation="0" small v-if="messageLogin === false"
-        >忘记密码？
-        </v-btn
-        >
-        <v-btn class="ma-2" elevation="0" small @click="$router.push('/register')"
-        >注册
+          >忘记密码？
+        </v-btn>
+        <v-btn
+          class="ma-2"
+          elevation="0"
+          small
+          @click="$router.push('/register')"
+          >注册
         </v-btn>
       </div>
       <v-btn class="ma-2" color="primary" block @click="login">登录</v-btn>
@@ -95,7 +103,7 @@
 </template>
 
 <script>
-import {loginByUsername} from "../api/user";
+import { loginByUsername } from "../api/user";
 
 export default {
   data() {
@@ -103,6 +111,7 @@ export default {
       username: "",
       password: "",
       verifyCode: "",
+      phone: "",
       showPassword: false,
       isSuccess: false,
       messageLogin: false,
@@ -111,7 +120,7 @@ export default {
       rules: {
         required: (value) => !!value || "必填.",
         phone: (v) =>
-            /^[1][3,4,5,7,8,9][0-9]{9}$/.test(v) || "手机号码格式不正确",
+          /^[1][3,4,5,7,8,9][0-9]{9}$/.test(v) || "手机号码格式不正确",
       },
     };
   },
@@ -120,13 +129,16 @@ export default {
       if (this.messageLogin) {
         this.loginByMessage();
       } else {
+        if (this.username === "" || this.password === "") {
+          this.alertMessage = "请输入用户名和密码";
+          this.snackbar = true;
+          return;
+        }
+
         this.$router.push("/class");
-        let result = await loginByUsername(this.username.trim(), this.password)
+        let result = await loginByUsername(this.username.trim(), this.password);
         console.log(result);
-        // .post("", {
-        //   username: this.username.trim(),
-        //   password: this.password,
-        // })
+
         // .then(function (res) {
         //   this.isSuccess = res.data.isSuccess;
         //   //判断登录成功，设置用户名
@@ -138,25 +150,21 @@ export default {
         //     this.snackbar = true;
         //   }
         // })
-        // .catch(function (error) {
-        //   console.log(error);
-        // });
-
-        // let loginResponse = await login(this.username.trim(), this.password);
-        // if (loginResponse.status === 200 && loginResponse.data.code === '200') {
-        //   this.$router.push("/class");
-        // } else {
-        //   console.log("登录失败");
-        // }
       }
     },
 
     async loginByMessage() {
-      // if (this.verifyCode === "") {
-      //   this.alertMessage = "请先获取验证码";
-      //   this.snackbar = true;
-      //   return;
-      // }
+      if (this.phone === "") {
+        this.alertMessage = "请填写正确的手机号";
+        this.snackbar = true;
+        return;
+      } else if (this.verifyCode === "") {
+        this.alertMessage = "请先获取验证码";
+        this.snackbar = true;
+        return;
+      }
+      // let result = await loginByUsername(this.phone.trim(), this.verifyCode);
+
       // service
       //   .post("", {
       //     username: this.username.trim(),
@@ -175,18 +183,9 @@ export default {
       //   .catch(function (error) {
       //     console.log(error);
       //   });
-      //
-      // // let loginResponse = await login(this.username.trim(), this.password);
-      // // if (loginResponse.status === 200 && loginResponse.data.code === '200') {
-      // //   this.$router.push("/class");
-      // // } else {
-      // //   console.log("登录失败");
-      // // }
     },
 
-    getVerifyCode() {
-
-    },
+    getVerifyCode() {},
   },
 };
 </script>
